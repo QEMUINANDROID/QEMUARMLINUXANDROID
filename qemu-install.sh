8682bec256f50b58d8f23ba0ca5e6eec44d442c8
niuyuling <niuyuling@github.com>
2016-11-23 22:45:57 +0800

    修改：      qemu-install.sh

--- qemu-install.sh
+++ qemu-install.sh
@@ -1,11 +1,14 @@
 #!/system/bin/sh
 #20160501 AIXIAO write.
 #20160616 AIXIAO modify.
-#Email 1605227279@qq.com
+#20161123 AIXIAO modify.
+#Email 1605227279@qq.com.
+
+qemu_init(){
+    bbox="/system/xbin/busybox"
+}
 
 qemu_install(){
-    bbox="/system/xbin/busybox"
-
     ${bbox} tar -xjvf /sdcard/qemu.tar.bz2 -C /
     ${bbox} mount -o remount,rw /
     ${bbox} mount -o remount,rw /system
@@ -14,4 +17,5 @@ qemu_install(){
     ${bbox} mount -o remount,ro /
 }
 
+qemu_init
 qemu_install
